c30ef691601e70c56701684175f1188dc0c7d50f
sfan <sfan_zzy@163.com>
2018-01-04 18:58:59 +0800

    Initial commit

---
+++ app/src/main/java/com/sfan/zzy/webapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.sfan.zzy.webapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView);
+        webView.loadUrl("file:///android_asset/html/html2017081401.html");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
